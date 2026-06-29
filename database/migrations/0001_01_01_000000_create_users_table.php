e98f21bb1d684e6f4d278a74e188682aa8a5eba6
KeyvanEA <keyvan80ebneali@gmail.com>
2026-06-29 19:27:17 +0330

    database: customize users migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,28 +13,33 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('full_name');
+            $table->string('mobile')->unique();
+            $table->string('student_number')->unique();
+            $table->string('field_of_study');
+            $table->string('university_name');
+            $table->text('bio');
+            $table->string('email')->unique()->nullable();
+            $table->string('avatar')->nullable();
+            $table->string('resume_file')->nullable();
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
-        });
+//        Schema::create('password_reset_tokens', function (Blueprint $table) {
+//            $table->string('email')->primary();
+//            $table->string('token');
+//            $table->timestamp('created_at')->nullable();
+//        });
+//
+//        Schema::create('sessions', function (Blueprint $table) {
+//            $table->string('id')->primary();
+//            $table->foreignId('user_id')->nullable()->index();
+//            $table->string('ip_address', 45)->nullable();
+//            $table->text('user_agent')->nullable();
+//            $table->longText('payload');
+//            $table->integer('last_activity')->index();
+//        });
     }
 
     /**
