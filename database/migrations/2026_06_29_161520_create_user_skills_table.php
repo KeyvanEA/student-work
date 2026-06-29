f287ec3d3349662c873586995835a814c5692c54
KeyvanEA <keyvan80ebneali@gmail.com>
2026-06-29 20:10:31 +0330

    database: create skills & user_skills  migration

--- database/migrations/2026_06_29_161520_create_user_skills_table.php
+++ database/migrations/2026_06_29_161520_create_user_skills_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('user_skills', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('user_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->foreignId('skill_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->unique(['user_id', 'skill_id']);
+
             $table->timestamps();
         });
     }
