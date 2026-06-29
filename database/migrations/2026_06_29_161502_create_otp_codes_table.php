058773d7b93f52890111f08ef98ec008f43b2f9d
KeyvanEA <keyvan80ebneali@gmail.com>
2026-06-29 19:55:19 +0330

    database: create otp_code  migration

---
+++ database/migrations/2026_06_29_161502_create_otp_codes_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('otp_codes', function (Blueprint $table) {
+            $table->id();
+            $table->string('mobile')->unique();
+            $table->char('code', 6);
+            $table->timestamp('created_at');
+            $table->timestamp('expires_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('otp_codes');
+    }
+};
