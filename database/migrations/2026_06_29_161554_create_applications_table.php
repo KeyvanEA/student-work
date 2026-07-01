2d8b276a41cabb2c82810923c8cde00918eae4af
KeyvanEA <keyvan80ebneali@gmail.com>
2026-07-01 18:01:36 +0330

    Add applications  table migration

--- database/migrations/2026_06_29_161554_create_applications_table.php
+++ database/migrations/2026_06_29_161554_create_applications_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('applications', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('task_id')->constrained()->cascadeOnDelete();
+            $table->unique(['user_id', 'task_id']);
+            $table->text('description');
+            $table->enum('status', ['pending','contacted', 'accepted', 'rejected'])->default('pending');
             $table->timestamps();
         });
     }
